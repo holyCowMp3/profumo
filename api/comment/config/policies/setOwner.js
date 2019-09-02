bcf4ae9f3a50f1b1ed624d5bf269a3c87f622b4d
user <hellios.dt@gmail.com>
2019-09-02 13:32:33 +0300

    Profumo commit

--- api/comment/config/policies/setOwner.js
+++ api/comment/config/policies/setOwner.js
@@ -2,14 +2,7 @@ module.exports = async (ctx, next) => {
 
   const { _id,name,gender } =ctx.state.user;
   const { body } = ctx.request;
-  console.log(ctx.state.user);
-  console.log(ctx.state.user.gender);
-  console.log(gender);
-  body.gender = await gender?ctx.state.user.gender+'':'female';
-  body.owner = await _id.toString();
-  body.name = await name.toString();
-  console.log(body);
-  ctx.request.body =  await body;
+  body.owner = _id.toString();
 
 
   await next();
