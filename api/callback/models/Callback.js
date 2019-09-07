744d8eb759abdddd7ed75385067bf05683a063dc
user <hellios.dt@gmail.com>
2019-09-07 12:38:52 +0300

    Fixed NovaPoshta, added saving of order and processing in liqpay

--- api/callback/models/Callback.js
+++ api/callback/models/Callback.js
@@ -12,60 +12,68 @@ module.exports = {
   // After saving a value.
   // Fired after an `insert` or `update` query.
   afterSave: async (model, result) => {
-    let liqPayConf = require('../../liqpayConf.json');
-    let novaPoshta = new NovaPoshta({apiKey: liqPayConf.nova_poshta});
-    let profumoCounterparty = '4187cb04-cd83-11e9-9937-005056881c6b';
-    Order.find({"_id":result.order_id}).then( order => {
-      // novaPoshta.counterparty.saveCounterparty({
-      //   FirstName: order.deliveryInfo.name,
-      //   MiddleName: order.deliveryInfo.surname,
-      //   LastName: order.deliveryInfo.thirdname,
-      //   Phone: order.deliveryInfo.phone,
-      //   Email: '',
-      //   CounterpartyType: 'PrivatePerson',
-      //   CounterpartyProperty: 'Recipient'
-      // }).then(counterparty => {
-      //   if (counterparty.success) {
-      //     let counterpartyId = counterparty.data.Ref;
-      //
-      //   }
-      // });
-      console.log(order)
-      // novaPoshta.document.saveInternetDocument(
-      //   {
-      //     NewAddress: '1',
-      //     PayerType: 'Sender',
-      //     PaymentMethod: 'Cash',
-      //     CargoType: 'Cargo',
-      //     VolumeGeneral: '0.1',
-      //     Weight: '10',
-      //     ServiceType: 'WarehouseWarehouse',
-      //     SeatsAmount: '1',
-      //     Description: 'Косметичні засоби',
-      //     Cost: '2000',
-      //     CitySender: '8d5a980d-391c-11dd-90d9-001a92567626',
-      //     Sender: profumoCounterparty,
-      //     SenderAddress: 'd492290b-55f2-11e5-ad08-005056801333',
-      //     ContactSender: profumoCounterparty,
-      //     SendersPhone: '380950831150',
-      //     RecipientCityName: order.deliveryInfo.cityName,
-      //     RecipientArea: '',
-      //     RecipientAreaRegions: '',
-      //     RecipientAddressName: order.deliveryInfo.postOfficeName,
-      //     RecipientHouse: '',
-      //     RecipientFlat: '',
-      //     RecipientName: order.deliveryInfo.name +' '+ order.deliveryInfo.surname,
-      //     RecipientType: 'PrivatePerson',
-      //     RecipientsPhone: order.deliveryInfo.phone,
-      //     DateTime: new Date().toLocaleDateString('en-GB', {
-      //       day : 'numeric',
-      //       month : 'numeric',
-      //       year : 'numeric'
-      //     }).split('/').join('.')
-      //   }
-      // );
 
-    });
+    switch (result.status) {
+      case 'success':
+      case 'wait_accept': {
+        let liqPayConf = require('../../liqpayConf.json');
+        let novaPoshta = new NovaPoshta({apiKey: liqPayConf.nova_poshta});
+        let profumoCounterparty = '4187cb04-cd83-11e9-9937-005056881c6b';
+        let order = await strapi.services.order.fetch({'_id': result.order_id});
+        let price = 0;
+        for (let product of order.orders) {
+          let productFromDb = await strapi.services.product.fetch({'_id': product.product.id});
+          let category = await strapi.services.category.fetch({'_id': productFromDb.category});
+          let minusPrice = 0;
+          if (category.discount) {
+            minusPrice = (productFromDb.price * (category.discount.percent / 100));
+          } else {
+            if (productFromDb.discounts) {
+              for (let disc of productFromDb.discounts) {
+                minusPrice += (disc.percent / 100) * (productFromDb.price - minusPrice);
+              }
+            }
+            price += ((productFromDb.discounts ? productFromDb.price - minusPrice : productFromDb.price) * product.count);
+          }
+        }
+        console.log(order);
+        function formatDate(date) {
+          var day = date.getDate();
+          var monthIndex = date.getMonth() + 1;
+          var year = date.getFullYear();
+          return day + '.' + monthIndex + '.' + year;
+        }
+        novaPoshta.document.saveInternetDocument(
+          {
+            NewAddress: '1',
+            PayerType: 'Sender',
+            PaymentMethod: 'Cash',
+            CargoType: 'Cargo',
+            VolumeGeneral: '0.1',
+            Weight: '10',
+            ServiceType: 'WarehouseWarehouse',
+            SeatsAmount: '1',
+            Description: 'Косметичні засоби',
+            Cost: price,
+            CitySender: 'e221d642-391c-11dd-90d9-001a92567626',
+            Sender: profumoCounterparty,
+            SenderAddress: '5a39e5a0-e1c2-11e3-8c4a-0050568002cf',
+            ContactSender: 'fb7ee3ed-cfc3-11e9-9937-005056881c6b',
+            SendersPhone: '380950831150',
+            RecipientCityName: order.deliveryInfo.cityName,
+            RecipientArea: '',
+            RecipientAreaRegions: '',
+            RecipientAddressName: order.deliveryInfo.postOfficeName,
+            RecipientHouse: '',
+            RecipientFlat: '',
+            RecipientName: order.deliveryInfo.name + ' ' + order.deliveryInfo.surname,
+            RecipientType: 'PrivatePerson',
+            RecipientsPhone: order.deliveryInfo.phone,
+            DateTime: formatDate(new Date())
+          }
+        ).then(json => json.data);
+      }
+    }
 
   },
 
