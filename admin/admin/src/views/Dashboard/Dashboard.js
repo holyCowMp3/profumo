3d86f991c44885b4b0fc8745da4eb1da48a0263a
teslov <hellios.dt@gmail.com>
2019-09-20 08:55:48 +0300

    Added additional data for entities

--- admin/admin/src/views/Dashboard/Dashboard.js
+++ admin/admin/src/views/Dashboard/Dashboard.js
@@ -1,9 +1,4 @@
 import React, {Component} from 'react';
-import CardDash from './CardDash';
-import Calendar from './Calendar';
-import LineChart from './LineChart';
-import Money from '@kiwicom/orbit-components/es/icons/Money';
-import {cloneDeep, findIndex, isArray} from 'lodash';
 
 class Dashboard extends Component {
 
@@ -16,132 +11,132 @@ class Dashboard extends Component {
       data: []
     };
   }
+  //
+  // getJwt() {
+  //   var headers = {
+  //     'Accept': 'application/json',
+  //     'Content-Type': 'application/json',
+  //     'Connection': 'keep-alive',
+  //   };
+  //   var dataString = '{"identifier":"test","password":"testtest"}';
+  //   return fetch('https://profumo.com.ua/auth/local',
+  //     {
+  //       method: 'POST',
+  //       headers: headers,
+  //       body: dataString,
+  //     }
+  //   ).then(body => body.json().jwt);
+  // }
 
-  getJwt() {
-    var headers = {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Connection': 'keep-alive',
-    };
-    var dataString = '{"identifier":"test","password":"testtest"}';
-    return fetch('https://profumo.com.ua/auth/local',
-      {
-        method: 'POST',
-        headers: headers,
-        body: dataString,
-      }
-    ).then(body => body.json().jwt);
-  }
-
-  getDataFromSite(jwt, body) {
-    var headers = {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + jwt,
-      'Connection': 'keep-alive',
-    };
-    return fetch('https://profumo.com.ua/graphql', {
-      method: 'POST',
-      headers: headers,
-      body: JSON.stringify({query: body})
-    }).then(body => body.json());
-  }
+  // getDataFromSite(jwt, body) {
+  //   var headers = {
+  //     'Accept': 'application/json',
+  //     'Content-Type': 'application/json',
+  //     'Authorization': 'Bearer ' + jwt,
+  //     'Connection': 'keep-alive',
+  //   };
+  //   return fetch('https://profumo.com.ua/graphql', {
+  //     method: 'POST',
+  //     headers: headers,
+  //     body: JSON.stringify({query: body})
+  //   }).then(body => body.json());
+  // }
 
   componentWillReceiveProps(nextProps, nextContext) {
     this.getStats();
     this.setState(nextProps);
   }
-
-  getOptions = query => {
-    const params = {
-      _limit: 20,
-      _start: this.state.start,
-      source: this.props.relation.plugin || 'content-manager',
-    };
-    // Set `query` parameter if necessary
-    if (query) {
-      delete params._limit;
-      delete params._skip;
-      params[`${this.props.relation.displayedAttribute}_contains`] = query;
-    }
-    // Request URL
-    let requestUrl = '/content-manager/stats';
-    // Call our request helper (see 'utils/request')
-    return request(requestUrl, {
-      method: 'GET',
-      params,
-    })
-      .then(response => {
-        console.log(response.length);
-        /* eslint-disable indent */
-        const options = isArray(response)
-          ? response.map(item => ({
-            value: item,
-            label: templateObject(
-              {mainField: this.props.relation.displayedAttribute},
-              item
-            ).mainField,
-          }))
-          : [
-            {
-              value: response,
-              label: response[this.props.relation.displayedAttribute],
-            },
-          ];
-        /* eslint-enable indent */
-        const newOptions = cloneDeep(this.state.options);
-        options.map(option => {
-          // Don't add the values when searching
-          if (
-            findIndex(newOptions, o => o.value.id === option.value.id) === -1
-          ) {
-            return newOptions.push(option);
-          }
-        });
-        return this.setState({
-          options: newOptions,
-          isLoading: false,
-        });
-      })
-      .catch(() => {
-        strapi.notification.error(
-          'content-manager.notification.error.relationship.fetch'
-        );
-      });
-  };
-
-  getStats = () => {
-    var jwt = '';
-    return this.getJwt().then(res => {
-      jwt = res;
-      this.getDataFromSite(res, `query{
-                                      usersConnection{
-                                     \t\tgroupBy{
-                                          createdAt{
-                                            key 
-                                          }
-                                        }
-                                      }
-                                    }`
-      ).then(res => res.data.usersConnection.groupBy.createdAt).map(
-        key => {
-          return {
-            x: key.toLocaleDateString(),
-            y: this.getDataFromSite(jwt, `
-            query{
-              usersConnection (where:{createdAt:"${key.toISOString()}"}){
-                aggregate{
-                  count
-                }
-              }        
-            }
-          `)
-          };
-        }
-      );
-    });
-
-  }
+  //
+  // getOptions = query => {
+  //   const params = {
+  //     _limit: 20,
+  //     _start: this.state.start,
+  //     source: this.props.relation.plugin || 'content-manager',
+  //   };
+  //   // Set `query` parameter if necessary
+  //   if (query) {
+  //     delete params._limit;
+  //     delete params._skip;
+  //     params[`${this.props.relation.displayedAttribute}_contains`] = query;
+  //   }
+  //   // Request URL
+  //   let requestUrl = '/content-manager/stats';
+  //   // Call our request helper (see 'utils/request')
+  //   return request(requestUrl, {
+  //     method: 'GET',
+  //     params,
+  //   })
+  //     .then(response => {
+  //       console.log(response.length);
+  //       /* eslint-disable indent */
+  //       const options = isArray(response)
+  //         ? response.map(item => ({
+  //           value: item,
+  //           label: templateObject(
+  //             {mainField: this.props.relation.displayedAttribute},
+  //             item
+  //           ).mainField,
+  //         }))
+  //         : [
+  //           {
+  //             value: response,
+  //             label: response[this.props.relation.displayedAttribute],
+  //           },
+  //         ];
+  //       /* eslint-enable indent */
+  //       const newOptions = cloneDeep(this.state.options);
+  //       options.map(option => {
+  //         // Don't add the values when searching
+  //         if (
+  //           findIndex(newOptions, o => o.value.id === option.value.id) === -1
+  //         ) {
+  //           return newOptions.push(option);
+  //         }
+  //       });
+  //       return this.setState({
+  //         options: newOptions,
+  //         isLoading: false,
+  //       });
+  //     })
+  //     .catch(() => {
+  //       strapi.notification.error(
+  //         'content-manager.notification.error.relationship.fetch'
+  //       );
+  //     });
+  // };
+  //
+  // getStats = () => {
+  //   var jwt = '';
+  //   return this.getJwt().then(res => {
+  //     jwt = res;
+  //     this.getDataFromSite(res, `query{
+  //                                     usersConnection{
+  //                                    \t\tgroupBy{
+  //                                         createdAt{
+  //                                           key
+  //                                         }
+  //                                       }
+  //                                     }
+  //                                   }`
+  //     ).then(res => res.data.usersConnection.groupBy.createdAt).map(
+  //       key => {
+  //         return {
+  //           x: key.toLocaleDateString(),
+  //           y: this.getDataFromSite(jwt, `
+  //           query{
+  //             usersConnection (where:{createdAt:"${key.toISOString()}"}){
+  //               aggregate{
+  //                 count
+  //               }
+  //             }
+  //           }
+  //         `)
+  //         };
+  //       }
+  //     );
+  //   });
+  //
+  // }
 
   render() {
     return (
