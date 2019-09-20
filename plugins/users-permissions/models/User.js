783f6adea98a98704e60154c3a4760e2714cd663
teslov <hellios.dt@gmail.com>
2019-09-20 21:37:02 +0300

    Added additional data for entities

--- plugins/users-permissions/models/User.js
+++ plugins/users-permissions/models/User.js
@@ -15,7 +15,7 @@ module.exports = {
   // After saving a value.
   // Fired after an `insert` or `update` query.
   afterSave: async (model, result) => {
-    strapi.services.cart.create({body:[],owner:result._id});
+    Cart.create({body:[],owner:result._id});
   },
 
   // Before fetching all values.
